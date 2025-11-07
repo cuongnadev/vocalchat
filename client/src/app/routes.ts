fce7b4349d247c03ba0496e267bce7947ffc249c
Cuong Dev <cuongna.dev@gmail.com>
2025-11-08 02:28:33 +0700

    feat(auth): complete entire UI auth(login, register, verify-code, profile-info)

--- client/src/app/routes.ts
+++ client/src/app/routes.ts
@@ -5,6 +5,8 @@ import Register from "@/pages/auth/Register";
 import ChatLayout from "./layout/ChatLayout";
 import AuthLayout from "./layout/AuthLayout";
 import { isLoggedIn } from "@/utils/auth";
+import VerifyCode from "@/pages/auth/VerifyCode";
+import ProfileInfo from "@/pages/auth/ProfileInfo";
 
 const rootRoute = createRootRoute();
 
@@ -44,6 +46,18 @@ const registerRoute = createRoute({
     component: Register,
 });
 
+const verifyCode = createRoute({
+  getParentRoute: () => authLayoutRoute,
+  path: "/register/verify-code",
+  component: VerifyCode
+})
+
+const profileInfo = createRoute({
+  getParentRoute: () => authLayoutRoute,
+  path: "/register/profile-info",
+  component: ProfileInfo
+})
+
 const rootRedirectRoute = createRoute({
   getParentRoute: () => rootRoute,
   path: "/",
@@ -56,5 +70,5 @@ const rootRedirectRoute = createRoute({
 export const routeTree = rootRoute.addChildren([
     rootRedirectRoute,
     chatLayoutRoute.addChildren([chatRoute]),
-    authLayoutRoute.addChildren([loginRoute, registerRoute]),
+    authLayoutRoute.addChildren([loginRoute, registerRoute, verifyCode, profileInfo]),
 ]);
